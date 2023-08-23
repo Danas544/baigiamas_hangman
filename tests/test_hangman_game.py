0524c10c367bcef449e2d28a2e7d2370ad26ce49
Danielius <Danasltuu@gmail.com>
2023-08-23 21:53:25 +0300

    niekas nepasikeite

--- tests/test_hangman_game.py
+++ tests/test_hangman_game.py
@@ -3,30 +3,32 @@ from unittest.mock import Mock
 from hangman.game_logic.hangman_game import HangmanGame
 from hangman.hangman_db.models.theme import Theme
 
-class TestHangmanGame(unittest.TestCase):
 
+class TestHangmanGame(unittest.TestCase):
     def setUp(self):
         self.game = HangmanGame()
         self.mock_db_session = Mock()
 
-
     def test_get_game_data(self):
         game_data = self.game.get_game_data()
-        self.assertEqual(game_data, {
-            "theme_id": None,
-            "theme_name": None,
-            "secret_word": "",
-            "guess_word": [],
-            "guessed_letters": [],
-            "guesses_left": 10,
-            "win": None,
-        })
+        self.assertEqual(
+            game_data,
+            {
+                "theme_id": None,
+                "theme_name": None,
+                "secret_word": "",
+                "guess_word": [],
+                "guessed_letters": [],
+                "guesses_left": 10,
+                "win": None,
+            },
+        )
 
     def test_select_theme_valid(self):
         theme_id = 1
         mock_theme = Theme(id=theme_id, name="Test Theme")
         self.game.THEMES = [mock_theme]
-        
+
         self.game.select_theme(theme_id)
         self.assertEqual(self.game.theme_id, theme_id)
         self.assertEqual(self.game.theme_name, "Test Theme")
@@ -34,16 +36,14 @@ class TestHangmanGame(unittest.TestCase):
     def test_select_theme_invalid(self):
         theme_id = 1
         self.game.THEMES = []
-        
+
         with self.assertRaises(ValueError):
             self.game.select_theme(theme_id)
-
-
 
     def test_guess_letter_correct(self):
         self.game.secret_word = "apple"
         self.game.guess_word = ["_", "_", "_", "_", "_"]
-        
+
         response, status_code = self.game.guess_letter("a")
         self.assertEqual(response, "Good guess!")
         self.assertEqual(status_code, 201)
@@ -60,8 +60,5 @@ class TestHangmanGame(unittest.TestCase):
         self.assertEqual(self.game.guesses_left, 9)
 
 
-
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
